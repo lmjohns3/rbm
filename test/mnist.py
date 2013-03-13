f4310d175ffd2e1559159a73674bac5d0fc5e870
Leif Johnson <leif@leifjohnson.net>
2013-03-13 18:29:25 -0500

    Update test for use with newer version of glumpy.

--- test/mnist.py
+++ test/mnist.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (c) 2011 Leif Johnson <leif@leifjohnson.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,45 +20,44 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
-import sys
-import time
-import numpy
-import glumpy
-import pickle
-import logging
-import datetime
-import optparse
+import argparse
 import collections
+import datetime
+import glumpy
+import logging
+import numpy as np
 import numpy.random as rng
+import os
+import pickle
+import sys
 
-from PIL import Image
-from OpenGL import GL as gl
-
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 import lmj.rbm
 import idx_reader
 
-FLAGS = optparse.OptionParser()
-FLAGS.add_option('', '--model', metavar='FILE',
+FLAGS = argparse.ArgumentParser(
+        conflict_handler='resolve',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+FLAGS.add_argument('--model', metavar='FILE',
                  help='load saved model from FILE')
-FLAGS.add_option('-i', '--images', metavar='FILE',
+FLAGS.add_argument('-i', '--images', metavar='FILE',
                  help='load image data from FILE')
-FLAGS.add_option('-l', '--labels', metavar='FILE',
+FLAGS.add_argument('-l', '--labels', metavar='FILE',
                  help='load image labels from FILE')
-FLAGS.add_option('-g', '--gaussian', action='store_true',
+FLAGS.add_argument('-g', '--gaussian', action='store_true',
                  help='use gaussian visible units')
-FLAGS.add_option('-r', '--learning-rate', type=float, default=0.1, metavar='K',
+FLAGS.add_argument('-c', '--conv', action='store_true',
+                 help='use a convolutional network')
+FLAGS.add_argument('-r', '--learning-rate', type=float, default=0.1, metavar='K',
                  help='use a learning rate of K')
-FLAGS.add_option('-m', '--momentum', type=float, default=0.2, metavar='K',
+FLAGS.add_argument('-m', '--momentum', type=float, default=0.2, metavar='K',
                  help='use a learning momentum of K')
-FLAGS.add_option('', '--l2', type=float, default=0.001, metavar='K',
+FLAGS.add_argument('--l2', type=float, default=0.001, metavar='K',
                  help='apply L2 regularization with weight K')
-FLAGS.add_option('-p', '--sparsity', type=float, default=0.1, metavar='K',
+FLAGS.add_argument('-p', '--sparsity', type=float, default=0.1, metavar='K',
                  help='set a target sparsity of K for hidden units')
-FLAGS.add_option('-n', '--n', type=int, default=10,
+FLAGS.add_argument('-n', '--n', type=int, default=10,
                  help='use NxN hidden units')
-FLAGS.add_option('-b', '--batch-size', type=int, default=5, metavar='N',
+FLAGS.add_argument('-b', '--batch-size', type=int, default=257, metavar='N',
                  help='process N images in one minibatch')
 
 
@@ -66,44 +67,42 @@ if __name__ == '__main__':
         level=logging.INFO,
         format='%(levelname).1s %(asctime)s [%(module)s:%(lineno)d] %(message)s')
 
-    opts, args = FLAGS.parse_args()
+    args = FLAGS.parse_args()
 
-    _visibles = numpy.zeros((opts.n, 28, 28), dtype=numpy.float32)
-    _hiddens = numpy.zeros((opts.n, opts.n), dtype=numpy.float32)
-    _weights = numpy.zeros((opts.n * opts.n, 28, 28), dtype=numpy.float32)
+    _visibles = np.zeros((args.n, 28, 28), dtype=np.float32)
+    _hiddens = np.zeros((args.n, args.n), dtype=np.float32)
+    _weights = np.zeros((args.n * args.n, 28, 28), dtype=np.float32)
 
     fig = glumpy.figure()
 
-    m = opts.gaussian and 0.5 or 1.5
     visibles = [glumpy.image.Image(v) for v in _visibles]
     hiddens = glumpy.image.Image(_hiddens)
-    weights = [glumpy.image.Image(w, vmin=-m, vmax=m) for w in _weights]
+    weights = [glumpy.image.Image(w) for w in _weights]
 
     visible_frames = [
-        fig.add_figure(opts.n + 1, opts.n, position=(opts.n, r)).add_frame(aspect=1)
-        for r in range(opts.n)]
+        fig.add_figure(args.n + 1, args.n, position=(args.n, r)).add_frame(aspect=1)
+        for r in range(args.n)]
 
     weight_frames = [
-        fig.add_figure(opts.n + 1, opts.n, position=(c, r)).add_frame(aspect=1)
-        for r in range(opts.n) for c in range(opts.n)]
+        fig.add_figure(args.n + 1, args.n, position=(c, r)).add_frame(aspect=1)
+        for r in range(args.n) for c in range(args.n)]
 
     loaded = False
-    updates = -1
     recent = collections.deque(maxlen=20)
     errors = [collections.deque(maxlen=20) for _ in range(10)]
-    testset = [None] * 10
     trainset = dict((i, []) for i in range(10))
-    loader = idx_reader.iterimages(opts.labels, opts.images, False)
+    loader = idx_reader.iterimages(args.images, args.labels, True)
 
-    rbm = opts.model and pickle.load(open(opts.model, 'rb')) or lmj.rbm.RBM(
-        28 * 28, opts.n * opts.n, not opts.gaussian)
+    Model = lmj.rbm.Convolutional if args.conv else lmj.rbm.RBM
+    rbm = args.model and pickle.load(open(args.model, 'rb')) or Model(
+        28 * 28, args.n * args.n, not args.gaussian)
 
-    trainer = lmj.rbm.Trainer(
-        rbm, l2=opts.l2, momentum=opts.momentum, target_sparsity=opts.sparsity)
+    Trainer = lmj.rbm.ConvolutionalTrainer if args.conv else lmj.rbm.Trainer
+    trainer = Trainer(rbm, l2=args.l2, momentum=args.momentum, target_sparsity=args.sparsity)
 
     def get_pixels():
         global loaded
-        if not loaded and numpy.all([len(trainset[t]) > 50 for t in range(10)]):
+        if not loaded and all(len(v) > 50 for v in trainset.itervalues()):
             loaded = True
 
         if loaded:
@@ -111,11 +110,7 @@ if __name__ == '__main__':
             pixels = trainset[t][rng.randint(len(trainset[t]))]
         else:
             t, pixels = loader.next()
-            if testset[t] is None and rng.random() < 0.3:
-                testset[t] = pixels
-                raise RuntimeError
-            else:
-                trainset[t].append(pixels)
+            trainset[t].append(pixels)
 
         recent.append(pixels)
         if len(recent) < 20:
@@ -124,24 +119,24 @@ if __name__ == '__main__':
         return pixels
 
     def flatten(pixels):
-        if not opts.gaussian:
+        if not args.gaussian:
             return pixels.reshape((1, 28 * 28)) > 30.
-        r = numpy.array(recent)
+        r = np.array(recent)
         mu = r.mean(axis=0)
-        sigma = numpy.clip(r.std(axis=0), 0.1, numpy.inf)
+        sigma = np.clip(r.std(axis=0), 0.1, np.inf)
         return ((pixels - mu) / sigma).reshape((1, 28 * 28))
 
     def unflatten(flat):
-        if not opts.gaussian:
+        if not args.gaussian:
             return 256. * flat.reshape((28, 28))
-        r = numpy.array(recent)
+        r = np.array(recent)
         mu = r.mean(axis=0)
         sigma = r.std(axis=0)
         return sigma * flat.reshape((28, 28)) + mu
 
     def learn():
-        batch = numpy.zeros((opts.batch_size, 28 * 28), 'd')
-        for i in range(opts.batch_size):
+        batch = np.zeros((args.batch_size, 28 * 28), 'd')
+        for i in range(args.batch_size):
             while True:
                 try:
                     pixels = get_pixels()
@@ -151,7 +146,7 @@ if __name__ == '__main__':
             flat = flatten(pixels)
             batch[i:i+1] = flat
 
-        trainer.learn(batch, learning_rate=opts.learning_rate)
+        trainer.learn(batch, learning_rate=args.learning_rate)
 
         logging.debug('mean weight: %.3g, vis bias: %.3g, hid bias: %.3g',
                       rbm.weights.mean(), rbm.vis_bias.mean(), rbm.hid_bias.mean())
@@ -165,24 +160,14 @@ if __name__ == '__main__':
             _visibles[i] = unflatten(v)
             [v.update() for v in visibles]
 
-            _hiddens[:] = h.reshape((opts.n, opts.n))
+            _hiddens[:] = h.reshape((args.n, args.n))
             hiddens.update()
 
-        for i in range(opts.n * opts.n):
+        for i in range(args.n * args.n):
             _weights[i] = rbm.weights[i].reshape((28, 28))
         [w.update() for w in weights]
 
         fig.redraw()
-
-    def evaluate():
-        for t, pixels in enumerate(testset):
-            if pixels is None:
-                continue
-            estimate = unflatten(rbm.reconstruct(flatten(pixels)))
-            errors[t].append(((pixels - estimate) ** 2).mean())
-        def mean(x):
-            return sum(x) / max(1, len(x))
-        logging.error(' : '.join('%d' % mean(errors[t]) for t in range(10)))
 
     @fig.event
     def on_draw():
@@ -196,24 +181,14 @@ if __name__ == '__main__':
 
     @fig.event
     def on_idle(dt):
-        global updates
-        if updates:
-            updates -= 1
-            update(*learn())
-            evaluate()
+        update(*learn())
 
     @fig.event
     def on_key_press(key, modifiers):
-        global updates
         if key == glumpy.window.key.ESCAPE:
             sys.exit()
         if key == glumpy.window.key.S:
             fn = datetime.datetime.now().strftime('rbm-%Y%m%d-%H%M%S.p')
             pickle.dump(rbm, open(fn, 'wb'))
-        if key == glumpy.window.key.SPACE:
-            updates = updates == 0 and -1 or 0
-        if key == glumpy.window.key.ENTER:
-            if updates >= 0:
-                updates = 1
 
     glumpy.show()
